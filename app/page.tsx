aafbe207d19962ddba90be82c5c040549e969cf3
Taavi Laanemaa <taavi.laanemaa@gmail.com>
2023-08-21 17:31:12 +0200

    Centered text

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <p className="text-9xl animate-bounce">🌈🦄🍩</p>
-      <p className="text-9xl mt-9 animate-bounce">Welcome to Iconic!</p>
+      <p className="text-9xl text-center mt-9 animate-bounce">Welcome to Iconic!</p>
     </main>
   );
 }
