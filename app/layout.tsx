13a16b36960a17e429b0d540eeccb0de3c70cfb5
Taavi Laanemaa <taavi.laanemaa@gmail.com>
2023-08-21 17:30:19 +0200

    Set up NextJS app

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Iconic',
+  description: 'Decode the message behind the emojis and unravel the words in this thrilling emoji guessing game!',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <p className="text-9xl animate-bounce">🌈🦄🍩</p>
+      <p className="text-9xl mt-9 animate-bounce">Welcome to Iconic!</p>
+    </main>
+  );
+}
